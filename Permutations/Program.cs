826b8b60433d7ce0a2e96a033612f53a8f15c3f9
Fred <domaine75@gmail.com>
2018-12-22 18:22:28 +0100

    Initial commit

---
+++ Permutations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Permutations
+{
+  class Program
+  {
+    static void Main()
+    {
+      string initialString = "123";
+      List<string> result = new List<string> { initialString };
+      string startSequence = initialString;
+      do
+      {
+        for (int i = initialString.Length - 1; i > 1; i--)
+        {
+          initialString = Permute(initialString, i);
+          result.Add(initialString);
+        }
+      } while (startSequence != initialString);
+
+      foreach (string item in result)
+      {
+        Console.WriteLine(item);
+      }
+
+      Console.WriteLine("Press any key to exit:");
+      Console.ReadKey();
+    }
+
+    private static string Permute(string theString, int position)
+    {
+      string tbackupChar = theString[position - 1].ToString();
+      string result = theString.Substring(0, theString.Length - 1);
+      result += theString[position] + theString[position - 1];
+      return result;
+    }
+  }
+}
